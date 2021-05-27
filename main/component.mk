6d7c1053005e091266632777342ce5d332afd41f
Chris Chatelain <chris@digiex.ca>
2021-05-27 16:32:30 -0500

    Reorganize build environment and troubleshoot some crashing

--- main/component.mk
+++ main/component.mk
@@ -2,5 +2,3 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_PRIV_INCLUDEDIRS = ./esp-idf-lib/components/hdd44780
-
